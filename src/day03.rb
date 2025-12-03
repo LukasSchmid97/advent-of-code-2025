0cce98a6f1b4b348f980743dc06408ba166ed2e0
lschmid <lukas.schmid@sitrox.com>
2025-12-03 11:33:11 +0100

    rewrite day3 for both parts

--- src/day03.rb
+++ src/day03.rb
@@ -8,14 +8,11 @@ File.open 'input/day03.txt' do |puzzle_in|
   input_string = puzzle_in.read
 end
 
-lines = input_string.split("\n")
-total_joltage = 0
-target_length = 12
-lines.each do |line|
+def get_highest_num(num_string, target_length)
   nums = []
-  line.chars.each_with_index do |char, i|
+  num_string.chars.each_with_index do |char, i|
     new_num = char.to_i
-    replace_limit = target_length - (line.size - i)
+    replace_limit = target_length - (num_string.size - i)
     replace_limit = [replace_limit, 0].max
     lesser = nums.collect { |n| n < new_num }
     if lesser[replace_limit..].any?
@@ -25,6 +22,17 @@ lines.each do |line|
       nums << new_num
     end
   end
-  total_joltage += nums.join.to_i
+  return nums.join.to_i
 end
-puts total_joltage
+
+lines = input_string.split("\n")
+total_joltage_part1 = 0
+total_joltage_part2 = 0
+
+lines.each do |line|
+  joltage_part2 = get_highest_num(line, 12)
+  total_joltage_part2 += joltage_part2
+  total_joltage_part1 += get_highest_num(joltage_part2.to_s, 2)
+end
+puts total_joltage_part1
+puts total_joltage_part2
